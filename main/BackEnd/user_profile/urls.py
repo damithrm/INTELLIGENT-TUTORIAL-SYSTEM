ae20afa2dbea57f327cf474e5a380027619e4d74
Damith Rathnayaka <87666673+damithrm@users.noreply.github.com>
2022-08-09 12:04:21 +0530

    Delete main directory

--- main/BackEnd/user_profile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import GetUserProfileView,UpdateUserProfileView
-
-urlpatterns = [
-    path('user',GetUserProfileView.as_view()),
-    path('update',UpdateUserProfileView.as_view()),
-    
-]
